def9659ec635c0529f519af65afd376a807a0f62
James Whale <james@james-whale.com>
2026-07-21 22:52:21 +0100

    feat: add NetBoxMacAddress model

--- src/models.py
+++ src/models.py
@@ -1,4 +1,7 @@
+from enum import StrEnum
 from json import dumps
+
+from pydantic import BaseModel, ConfigDict, computed_field
 
 
 class NetBoxError(Exception):
@@ -21,4 +24,27 @@ class NetBoxError(Exception):
             "error": self.error,
             "status_code": self.status_code,
         })
+
+
+class NetBoxAssignedObjectTypes(StrEnum):
+    DCIM_INTERFACE = "dcim.interface"
+    VIRTUALIZATION_VMINTERFACE = "virtualization.vminterface"
+
+
+class NetBoxBaseModel(BaseModel):
+    id: int = 1
+    _base_url: str = 'https://netbox.example.com'
+
+
+class NetBoxMacAddress(NetBoxBaseModel):
+    mac_address: str = '00:11:22:AA:BB:CC'
+    assigned_object_type: NetBoxAssignedObjectTypes
+    assigned_object_id: int = 1
+
+    @computed_field
+    @property
+    def url(self) -> str:
+        return f"{self._base_url}/api/dcim/mac_addresses/{self.id}"
+
+    model_config = ConfigDict(extra="allow", use_enum_values=True)
 
